bd38102a1733d01b932ea22dd98e49cc48ed5187
Alex Dinh <anhdinh17@gmail.com>
2021-11-10 22:17:03 -0600

    Set up Managers folder

--- TikTok/Managers/StorageManager.swift
+++ TikTok/Managers/StorageManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+    public static let shared = StorageManager()
+    
+    private let storage = Storage.storage().reference()
+    
+    private init(){}
+    
+    public func getVideoUrl(with identifier: String, completion: @escaping (URL) -> Void){
+        
+    }
+    
+    public func uploadVideoURL(from url: URL){
+        
+    }
+}
