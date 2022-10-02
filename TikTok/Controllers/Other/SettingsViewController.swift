304c0f3d447ecc7d9bd2689833e5c7c824357b70
Alex Dinh <anhdinh17@gmail.com>
2022-10-02 15:17:29 -0500

    Signing out
    setup SettingsVC to sign out

--- TikTok/Controllers/Other/SettingsViewController.swift
+++ TikTok/Controllers/Other/SettingsViewController.swift
@@ -7,36 +7,89 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    lazy var signOutButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("SIGN OUT", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.addTarget(self, action: #selector(didTapSignOut), for: .touchUpInside)
-        button.backgroundColor = .blue
-        return button
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
     }()
-    
+   
+//MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Settings"
-        view.addSubview(signOutButton)
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        signOutButton.frame = CGRect(x: 30, y: 250, width: view.width - 60, height: 50)
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    @objc func didTapSignOut(){
-        AuthManager.shared.signOut { [weak self] success in
-            if success {
-                let vc = SignInViewController()
-                navigationController?.pushViewController(vc, animated: true)
-            }
-        }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        createFooter()
     }
     
+    func createFooter(){
+        let footer = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: 100))
+        let button = UIButton(frame: CGRect(x: (view.width - 100)/2, y: 25, width: 100, height: 50))
+        button.setTitle("Sign Out", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(didTapSignOut), for: .touchUpInside)
+        footer.addSubview(button)
+        tableView.tableFooterView = footer
+    }
+    
+    @objc func didTapSignOut(){
+        let alert = UIAlertController(title: "Sign Out",
+                                      message: "Are you sure you want to sign out?",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Sign Out", style: .destructive) { [weak self] _ in
+            DispatchQueue.main.async {
+                AuthManager.shared.signOut { success in
+                    if success {
+                        // set values in UserDefaults to be nil before signing out
+                        UserDefaults.standard.setValue(nil, forKey: "username")
+                        UserDefaults.standard.setValue(nil, forKey: "profile_picture_url")
+                        
+                        // navigate to SignInVC
+                        let vc = SignInViewController()
+                        let navVC = UINavigationController(rootViewController: vc)
+                        navVC.modalPresentationStyle = .fullScreen
+                        self?.present(navVC, animated: true)
+                        
+                        // switch tab bar to default setting which is first tab (home tab)
+                        self?.navigationController?.popToRootViewController(animated: true)
+                        self?.tabBarController?.selectedIndex = 0
+                        
+                    } else {
+                        let alert = UIAlertController(title: "Sorry", message: "Unable to sign out, please try again.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                        self?.present(alert,animated: true)
+                    }
+                }
+            }
+        })
+        present(alert, animated: true)
+    }
+    
+}
+
+//MARK: - TableView
+extension SettingsViewController {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "YO!"
+        return cell
+    }
 }
